f8931ad610d992711c15bad595bafdae94a291d6
Edmund Yau <mc4u.hk@gmail.com>
2021-05-26 21:18:16 +0800

    more example

---
+++ src/test/java/hk/mc4u/webtester/test/Test03.java
@@ -0,0 +1,45 @@
+package hk.mc4u.webtester.test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Test03 {
+	WebDriver driver = new FirefoxDriver();
+
+	@Test
+	public void test01() throws Exception {
+		Object[][] data = {
+				{"A","a"},	
+				{"A","b"},	
+				{"A", Keys.DOWN},	
+				{"A", Keys.DOWN},	
+				{"A", Keys.DOWN},	
+				{"A","d"},	
+				{"A","e"},	
+		};
+		
+		List<Object[]> list = Arrays.asList(data);
+		
+		list.forEach(this::process);
+	}
+	
+	public void process(Object... params) {
+		log.info("Length: {}", params.length);
+		String name=(String) params[0];
+		CharSequence value=(CharSequence) params[1];
+		WebElement e = driver.findElement(By.name(name));
+		e.sendKeys(value);
+	}
+
+
+}
